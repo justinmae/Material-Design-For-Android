1d536fecc0261579fdb25582d805e8a9ee365b59
Justin Mae <justin511@gmail.com>
2016-05-03 13:09:55 -0400

    practice 2.18 add circular reveal effect when clicked

--- 2.18-CircularReveal/CircularReveal/app/src/main/java/materialdesign/udacity/com/circularreveal/MainActivity.java
+++ 2.18-CircularReveal/CircularReveal/app/src/main/java/materialdesign/udacity/com/circularreveal/MainActivity.java
@@ -1,7 +1,10 @@
 package materialdesign.udacity.com.circularreveal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void circularReveal(View view) {
+//        Button view = (Button) findViewById(R.id.button);
+
+        float finalRadius = (float) Math.hypot(view.getWidth()/2, view.getHeight()/2);
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(
+                view, view.getWidth()/2, view.getHeight()/2, 0, finalRadius);
+        anim.start();
     }
 
 
