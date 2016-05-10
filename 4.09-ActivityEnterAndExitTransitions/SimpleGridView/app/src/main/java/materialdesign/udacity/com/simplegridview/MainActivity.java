53eb27969f38e0925fb787b97f8512b0b948bb1a
Justin Mae <justin511@gmail.com>
2016-05-10 13:46:09 -0400

    Practice building simple GridViews

---
+++ 4.09-ActivityEnterAndExitTransitions/SimpleGridView/app/src/main/java/materialdesign/udacity/com/simplegridview/MainActivity.java
@@ -0,0 +1,52 @@
+package materialdesign.udacity.com.simplegridview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+//    String[] dataVal = new String[]{"A","B"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+
+        gridView.setAdapter(new GridAdapter());
+    }
+
+    private class GridAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 20;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.grid_item, parent, false);
+            }
+            ((TextView) convertView).setText("A");
+            return convertView;
+        }
+    }
+
+}
